c8488476344a9d545250aaa1a80f90471b9b5c75
parthivsurya <parthivsuryakb@gmail.com>
2025-12-26 19:53:16 +0530

    once again

---
+++ pathsum.py
@@ -0,0 +1,18 @@
+from pyparsing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+        if root.left is None and root.right is None:
+            return targetSum == root.val
+        remaining = targetSum - root.val
+        return (
+            self.hasPathSum(root.left, remaining) or
+            self.hasPathSum(root.right, remaining)
+        )
